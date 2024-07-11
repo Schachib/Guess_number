0143c5ca799521f9bcd55e7590d1e53f0b2c536d
Шамхалов Хабиб <h0491057@gmail.com>
2024-07-11 22:14:39 +0300

    rename

--- guess_number.py
+++ guess_number.py
@@ -1,15 +1,17 @@
 import random
 """Комментрай"""
+
+
 def guess_number():
     number = random.randint(1, 100)
     tries = 0
-    
-    print("Привет! Давай поиграем в угадай число. Я загадал число от 1 до 100.")
-    
+
+    print("Давай поиграем в угадай число. Я загадал число от 1 до 100.")
+
     while True:
         guess = int(input("Угадай число: "))
         tries += 1
-        
+
         if guess < number:
             print("Загаданное число больше")
         elif guess > number:
@@ -25,5 +27,5 @@ def guess_number():
         print("Спасибо за игру!")
         pass
 
-guess_number()
 
+guess_number()
